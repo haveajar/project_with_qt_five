8bd56f6a17bfc57697efcdb56c269bee2f77d213
haveajar <c.olin.thomson99@gmail.com>
2023-07-26 13:35:14 +0200

    Now have a working program that creates a big button that prints a line into the console.

--- my_qt_program.py
+++ my_qt_program.py
@@ -1,2 +1,26 @@
-if __name__ == '__main__':
+import sys
+
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+def button_pressed():
     print('Weeee!')
+
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Testapplikation")
+        button = QPushButton("Drücke mich!")
+        self.setCentralWidget(button)
+        button.clicked.connect(button_pressed)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+
+    app.exec()
